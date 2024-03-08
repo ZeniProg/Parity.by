2e88b01dcebfc9cf66bd8e8a77ef94001b892412
Kustiche <ChernichnyKustik@mail.ru>
2024-03-08 09:26:30 +0300

    Сверстал секцию "information-search"

--- assets/js/_functions.js
+++ assets/js/_functions.js
@@ -1,4 +1,5 @@
 import { header } from '../components/header/header';
 import { swiper } from './functions/swiper-init';
+import { select } from './functions/selects-init';
 import { spollers } from '../components/spollers/spollers';
 import { tabs } from '../components/tabs/tabs';
